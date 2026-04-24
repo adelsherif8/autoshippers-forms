463a274ebd2f936b7a11f50fa9c0dbafddc6e876
Adel Emad <adelsherif8@gmail.com>
2026-04-24 12:48:50 +0300

    1.0.16

--- autoshippers-forms.php
+++ autoshippers-forms.php
@@ -3,7 +3,7 @@
  * Plugin Name:  AutoShippers Forms
  * Plugin URI:   https://upwork.com/freelancers/adelsherif8
  * Description:  Multi-step Vehicle Shipping Quote form with GoHighLevel CRM integration.
- * Version:      1.0.15
+ * Version:      1.0.16
  * Author:       Adel Emad
  * Author URI:   https://upwork.com/freelancers/adelsherif8
  * License:      GPL-2.0+
@@ -12,7 +12,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'AS_VERSION',  '1.0.15' );
+define( 'AS_VERSION',  '1.0.16' );
 define( 'AS_DIR',         plugin_dir_path( __FILE__ ) );
 define( 'AS_URL',         plugin_dir_url( __FILE__ ) );
 define( 'AS_BASENAME',    plugin_basename( __FILE__ ) );
